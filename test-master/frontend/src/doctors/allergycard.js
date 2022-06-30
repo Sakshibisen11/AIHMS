dd77640cad74882c70017b1452c3882ad9cb52a1
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-07-01 03:35:20 +0530

    AIHMS

---
+++ test-master/frontend/src/doctors/allergycard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Akira from "../jj.jpg";
+import "../css/DoctorListCard.css";
+
+const AllergyCard = (props) => {
+  return (
+    <div>
+      <Card border="info" className="DoctorListCards">
+        <br />
+        <font className="DoctorIcon">
+          <i className="fas fa-books-medical fa-9x"></i>
+        </font>
+        <Card.Body className="Allergy">
+          <Card.Title style={{ color: "#195a65" }}>Report</Card.Title>
+          <Card.Text>
+            <div>Title: {props.title} </div>
+            <div>Place:{props.place}</div>
+            <div>Date:{props.date}</div>
+            <div>Description:{props.description}</div>
+            <div>Medication:{props.meds}</div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default AllergyCard;
